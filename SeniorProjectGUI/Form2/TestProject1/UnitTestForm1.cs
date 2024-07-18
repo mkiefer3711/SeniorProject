f2f527d309c0717ddbb9df61192d57b346794448
mkiefer3711 <74162397+mkiefer3711@users.noreply.github.com>
2024-07-18 15:24:29 -0400

    Add files via upload

---
+++ SeniorProjectGUI/Form2/TestProject1/UnitTestForm1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeniorProjectGUI;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTestForm1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
